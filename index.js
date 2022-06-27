49e853a3df11c70bb0808e5b943b8f1e8c83c0e8
Anwar1477 <joy.cse017@gmail.com>
2022-06-28 01:56:11 +0600

    checked

--- index.js
+++ index.js
@@ -2,22 +2,15 @@ const express = require('express');
 const cors = require('cors');
 let app = express();
 require('dotenv').config();
-let port = process.env.PORT || 8000 
+let port = process.env.PORT || 8000;
 
-app.use(cors())
-app.use(express.json())
-
-
-
-
-
-
-
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Server On Live')
-})
+  res.send('Server On Live');
+});
 
-app.listen(port, ()=> {
-    console.log(' Mojar Dokan Server Running ')
-})
+app.listen(port, () => {
+  console.log(' Mojar Dokan Server Running ');
+});
