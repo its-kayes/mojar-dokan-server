7d9034f8c23238ab49280831ac3b847c7b76ad9f
its-kayes <kayes.ek8@gmail.com>
2022-06-28 01:37:01 +0600

    MongoDB setup

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 let app = express();
@@ -9,8 +10,21 @@ app.use(express.json())
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n5jea.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run () {
+    try {
+        await client.connect();
+
+
+    }
+    finally{
+
+    }
+} 
 
 
 
